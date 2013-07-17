ac68bee88856565db320f81689910cc4be55ff4f
ld, wang <ldwangkimo@gmail.com>
2013-07-17 12:51:09 +0800

    learn.php

---
+++ PHPlearn.php
@@ -0,0 +1,41 @@
+<?php
+/*
+檔案 資料夾命名，不可用中文
+
+行註解(//) 或 塊註解( / * * /) 可以註解掉class成功
+
+
+
+
+
+
+
+
+課本-程式設計心法：1....
+
+ no  魔術數字;;
+     預防 "Devided by Zero"
+ no  case -> 01 x=a/b
+ yes case -> 01 if(0==b){ x=0 }
+          -> 02 else    { x=a/b };;
+ no 不同型別的變數做比較;;
+    留意 Compiler 的警告訊息;;
+
+
+
+
+
+
+課本-PHP物件導向的第一課：....
+
+class mydemo
+{
+    function hello()
+    {
+        echo("哈囉 世界!");
+    }
+}
+$mydemo = new mydemo;
+$mydemo ->hello();
+*/
+?>
